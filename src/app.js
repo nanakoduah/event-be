6398912a30b496128fd6ecd8bc8a803b29cf7eda
Nana koduah <nanakoduahfrimpong@yahoo.com>
2022-06-20 19:51:18 +0000

    routes and cors setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
-const dotenv = require('dotenv');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
+const globalErrorHandle = require('./errors/errorController');
+const AppError = require('./errors/AppError');
+const authRoutes = require('./v1/auth/authRoutes');
+const eventRoutes = require('./v1/events/eventRoutes');
+const categoryRoutes = require('./v1/categories/categoryRoutes');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+}
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/events', eventRoutes);
+app.use('/api/v1/categories', categoryRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find requested resource, ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandle);
 
 module.exports = app;
